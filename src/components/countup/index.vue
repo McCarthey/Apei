b8a9e08dcfab60b6c0d04816adddde90df0e8598
Randall <randall@duomai.com>
2019-04-02 15:42:53 +0800

    [fix] countup.js V1 to V2

--- src/components/countup/index.vue
+++ src/components/countup/index.vue
@@ -1,62 +1,60 @@
 <script>
-import Countup from 'countup.js'
+import { CountUp } from 'countup.js'
 
 export default {
-	name: 'VpCountup',
-	props: {
-		tag: {
-			type: String,
-			default: 'span',
-		},
-		start: {
-			type: Boolean,
-			default: true,
-		},
-		startVal: {
-			type: Number,
-			default: 0,
-		},
-		endVal: {
-			type: Number,
-			required: true,
-		},
-		// number of decimal places in number
-		decimals: {
-			type: Number,
-			default: 0,
-		},
-		// duration in seconds
-		duration: {
-			type: Number,
-			default: 2,
-		},
-		options: {
-			type: Object,
-			default() {
-				return {}
-			},
-		},
-	},
-	watch: {
-		start(val) {
-			if (val) {
-				this._countup.start()
-			}
-		},
-		endVal(val) {
-			this._countup.update(val)
-		}
-	},
-	mounted() {
-		this.$nextTick(() => {
-			this._countup = new Countup(this.$el, this.startVal, this.endVal, this.decimals, this.duration, this.options)
-			if (this.start) {
-				this._countup.start()
-			}
-		})
-	},
-	render(h) {
-		return h(this.tag, {}, [this.startVal])
-	}
+    name: 'VpCountup',
+    props: {
+        tag: {
+            type: String,
+            default: 'span',
+        },
+        start: {
+            type: Boolean,
+            default: true,
+        },
+        startVal: {
+            type: Number,
+            default: 0,
+        },
+        endVal: {
+            type: Number,
+            required: true,
+        },
+        // number of decimal places in number
+        decimals: {
+            type: Number,
+            default: 0,
+        },
+        // duration in seconds
+        duration: {
+            type: Number,
+            default: 2,
+        },
+    },
+    watch: {
+        start(val) {
+            if (val) {
+                this._countup.start()
+            }
+        },
+        endVal(val) {
+            this._countup.update(val)
+        },
+    },
+    mounted() {
+        this.$nextTick(() => {
+            this._countup = new CountUp(this.$el, this.endVal, {
+                startVal: this.startVal,
+                decimalPlaces: this.decimals,
+                duration: this.duration,
+            })
+            if (this.start) {
+                this._countup.start()
+            }
+        })
+    },
+    render(h) {
+        return h(this.tag, {}, [this.startVal])
+    },
 }
 </script>
