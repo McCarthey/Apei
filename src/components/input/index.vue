aa8199219cb9903a80eb4ba129ee6abf54395be6
Randall <randall@duomai.com>
2019-04-02 12:45:58 +0800

    [add] input

---
+++ src/components/input/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="weui-cell">
+        <div class="weui-cell__hd">
+            <label class="weui-label"></label>
+        </div>
+        <div class="weui-cell__bd">
+            <input type="text" class="weui-input" pattern="" placeholder="">
+        </div>
+    </div>
+</template>
