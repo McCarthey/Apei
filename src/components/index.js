34c0eb89188223acbb6a43efbad9f8ca50b2a2d6
Randall <randall@duomai.com>
2019-04-03 11:10:48 +0800

    [add] export all current components; publish v0.1.3

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import VpButton from './button'
+import VpInput from './input'
+import VpSwitch from './switch'
+import VpCountup from './countup'
+import VpLoading from './loading'
+import VpToast from './toast'
+import VpBadge from './badge'
 
 const Components = {
     VpButton,
+    VpInput,
+    VpSwitch,
+    VpCountup,
+    VpLoading,
+    VpToast,
+    VpBadge
 }
 
 Object.keys(Components).forEach(name => {
