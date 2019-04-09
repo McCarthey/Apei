09350c050101117b0935ca4c36dd39c178b447a8
Randall <randall@duomai.com>
2019-04-09 11:26:09 +0800

    [add] add import on-demand

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// import {Input} from 'a-pei'
+
+// Vue.use(Input)
 
 Vue.config.productionTip = false
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
-import VpButton from './components/button/index.js'
-import VpInput from './components/input/index.js'
-import VpSwitch from './components/switch/index.js'
-import VpCountup from './components/countup/index.js'
-import VpLoading from './components/loading/index.js'
-import VpToast from './components/toast/index.js'
-import VpBadge from './components/badge/index.js'
+import VpButton from "./components/button/index.js";
+import VpInput from "./components/input/index.js";
+import VpSwitch from "./components/switch/index.js";
+import VpCountup from "./components/countup/index.js";
+import VpLoading from "./components/loading/index.js";
+import VpToast from "./components/toast/index.js";
+import VpBadge from "./components/badge/index.js";
 
 const Components = {
-    VpButton,
-    VpInput,
-    VpSwitch,
-    VpCountup,
-    VpLoading,
-    VpToast,
-    VpBadge,
-}
+  VpButton,
+  VpInput,
+  VpSwitch,
+  VpCountup,
+  VpLoading,
+  VpToast,
+  VpBadge
+};
 
 const install = function(Vue) {
-    Object.keys(Components).forEach(name => {
-        Vue.component(name, Components[name])
-    })
+  Object.keys(Components).forEach(name => {
+    Vue.component(name, Components[name]);
+  });
+};
+
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
 
-if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue)
-}
-
-export default { install, ...Components }
+export default {
+  install,
+  VpButton,
+  VpInput,
+  VpSwitch,
+  VpCountup,
+  VpLoading,
+  VpToast,
+  VpBadge
+};
