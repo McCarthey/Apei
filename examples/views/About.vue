76ebc8eae4281521dad9c75d23ddd164eda24c2b
Randall <randall@duomai.com>
2019-04-25 17:13:12 +0800

    [test] export tool function

--- examples/views/About.vue
+++ examples/views/About.vue
@@ -3,3 +3,16 @@
         <h1>This is an about page</h1>
     </div>
 </template>
+
+<script>
+import throttle from "../../lib/throttle.js";
+export default {
+    created() {
+        window.onmousemove = throttle(() => {
+            console.log("eee" + +new Date());
+        }, 1000);
+        console.log("test", throttle);
+    }
+};
+</script>
+
